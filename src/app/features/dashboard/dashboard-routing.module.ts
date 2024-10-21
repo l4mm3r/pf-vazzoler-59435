0958e70915b8839cf82e32a86f5f0c4c5b52b995
Eduardo Vazzoler <eduardotoril@gmail.com>
2024-10-21 09:41:51 -0300

    ADD, routing to courses an link sidenav

--- src/app/features/dashboard/dashboard-routing.module.ts
+++ src/app/features/dashboard/dashboard-routing.module.ts
@@ -12,6 +12,11 @@ const routes: Routes = [
       import('./students/students.module').then((m) => m.StudentsModule),
   },
   {
+    path: 'courses',
+    loadChildren: () =>
+      import('./courses/courses.module').then((m) => m.CoursesModule),
+  },
+  {
     path: '**',
     redirectTo: 'home',
   },
