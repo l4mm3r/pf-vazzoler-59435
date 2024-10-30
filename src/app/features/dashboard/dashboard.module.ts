70fa09cabab5302531a76fb5b5f12d4d95dd6610
Eduardo Vazzoler <eduardotoril@gmail.com>
2024-10-30 16:52:57 -0300

    REFACTOR-button+add-layout

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
-
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
